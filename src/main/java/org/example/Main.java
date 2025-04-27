eff59a749b31b530c6e16b7cbdb57832a0802c22
Мария Мизина <ivanutkevich@gmail.com>
2025-04-27 18:44:13 +0300

    Add MyArrayList

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -2,6 +2,20 @@ package org.example;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+        MyArrayList<Integer> a = new MyArrayList();
+        a.add(5);
+        a.add(4);
+        a.add(3);
+        a.add(2);
+        a.add(1);
+        for(int i = 0; i < a.size(); i++){
+            System.out.println(a.get(i));
+        }
+
+        a.remove(2);
+
+        for(int i = 0; i < a.size(); i++){
+            System.out.println(a.get(i));
+        }
     }
 }
--- src/main/java/org/example/MyArrayList.java
+++ src/main/java/org/example/MyArrayList.java
@@ -1,4 +1,42 @@
 package org.example;
 
 public class MyArrayList<T> {
+    private static final int INIT_SIZE = 5;
+    private Object[] data = new Object[INIT_SIZE];
+    private int indexLast = -1;
+
+    public void add(T item){
+        if(indexLast == data.length - 1){
+            resize(data.length * 2);
+        }
+
+        data[++indexLast] = item;
+    }
+
+    public void remove(int index){
+        if(index == indexLast){
+            data[index] = null;
+            indexLast--;
+            return;
+        }
+
+        for(int i = index; i < indexLast; i ++){
+            data[i] = data[i + 1];
+        }
+
+        data[indexLast--] = null;
+    }
+
+    public T get(int index){
+        return (T)data[index];
+    }
+
+    public int size(){
+        return indexLast + 1;
+    }
+    private void resize(int newLength){
+        Object[] newData = new Object[newLength];
+        System.arraycopy(data, 0, newData, 0, indexLast);
+        data = newData;
+    }
 }
